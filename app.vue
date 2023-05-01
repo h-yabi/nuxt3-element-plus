33b36c1a4e67d117ca8176a42cd19a49f8ed1c46
hiroya.yabiku <hiroya.yabiku@prtimes.co.jp>
2023-05-01 19:09:01 +0900

    sass 設定

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <el-button @click="ElMessage('hello')">button</el-button>
+  <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+  <LazyElButton type="warning">lazy button</LazyElButton>
+  <p>test</p>
 </template>
+
+<style lang="scss">
+$primary-color: #1aad90;
+
+h2 {
+  font-size: large;
+  font-weight: bold;
+}
+p {
+  color: $primary-color;
+  box-shadow: $shadow-btn-basic;
+  margin: $margin-default;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: ['@element-plus/nuxt'],
+  css: ['@/assets/css/common.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/css/variables.scss";',
+        },
+      },
+    },
+  },
+});
